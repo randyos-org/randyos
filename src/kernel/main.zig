6406d1e31250136249ec8b71cdb8e49baf5116bb
Samuel Fiedler <samuel.fiedler@proton.me>
2023-12-21 19:02:02 +0100

    Add kernel panics

--- src/kernel/main.zig
+++ src/kernel/main.zig
@@ -1,6 +1,7 @@
 //! Simple Kernel
 //! 2023 by Samuel Fiedler
 
+const builtin = @import("std").builtin;
 const boot_info = @import("./boot_info.zig");
 const uart = @import("./uart.zig");
 
@@ -14,6 +15,18 @@ export fn kmain(kernel_boot_info: boot_info.KernelBootInfo) void {
     uart.uart_puts("\x1b[2J");
     // put out something
     uart.uart_puts("Hello World from kernel!\n");
+    @panic("Test Panic");
     // hang
+    // while (true) {}
+}
+
+/// Handle kernel panics
+pub fn panic(msg: []const u8, stack_trace: ?*builtin.StackTrace, return_address: ?usize) noreturn {
+    _ = stack_trace;
+    _ = return_address;
+    uart.puts("\n=============\n");
+    uart.puts("Kernel Panic!\n");
+    uart.puts(msg);
+    uart.puts("\n");
     while (true) {}
 }
