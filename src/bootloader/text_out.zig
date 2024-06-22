0728b350695cf0a7aee7287eafcc48a96db8aec4
Samuel Fiedler <samuel.fiedler@proton.me>
2024-06-22 07:28:51 +0200

    bootloader/text_out: explain

--- src/bootloader/text_out.zig
+++ src/bootloader/text_out.zig
@@ -1,30 +1,44 @@
 //! This module provides simple text output for UEFI
-//! 2023 by Samuel Fiedler
 
+// Again, our standard library
 const std = @import("std");
 const uefi = std.os.uefi;
 
+// We want to have a global console output protocol.
 var con_out: *uefi.protocol.SimpleTextOutput = undefined;
-var already_called_puts: u8 = 0;
+// But because that protocol is undefined at the beginning, we need to fill it (at runtime).
+// This is why we need this variable.
+var already_called_puts: bool = false;
 
-/// Put out any string
-///   - msg: the string to put out
+/// This function puts out any normal string.
 pub fn puts(msg: []const u8) void {
-    if (already_called_puts == 0) {
+    // If this is the first time this function was called, we will do some setup.
+    if (already_called_puts == false) {
+        // We save the console output protocol from the system table (so we don't have to locate it).
         con_out = uefi.system_table.con_out.?;
+        // We reset the screen…
         _ = con_out.reset(false);
-        already_called_puts = 1;
+        // And we set our variable to true, so we enter this condition only once.
+        already_called_puts = true;
     }
+    // Then, we iterate over the message we want to print out.
     for (msg) |c| {
+        // For each character, we convert it to a null-terminated 16bit string.
         const c_ = [1:0]u16{c};
-        _ = con_out.outputString(@as(*const [1:0]u16, &c_));
+        // And then, we output that string.
+        _ = con_out.outputString(&c_);
     }
 }
 
-/// Put out any formatted string
+/// This function puts out any formatted string, like std.debug.print does it.
 pub fn printf(comptime fmt: []const u8, args: anytype) void {
+    // Because I don't want to allocate, I just have this buffer as a "limit".
     var buf: [256]u8 = undefined;
+    // This is where I save the message.
     var msg: []u8 = undefined;
+    // Now, we call a function from the standard library. It writes the string resulting from the format string and the arguments
+    // into the buffer, but also returns a slice pointing to everything useful.
     msg = std.fmt.bufPrint(&buf, fmt, args) catch unreachable;
+    // And now, we just call our normal string output function.
     puts(msg);
 }
