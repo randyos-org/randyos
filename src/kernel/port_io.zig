11e7a8a012f5d361909ff3a41ef77648dafe0ffa
Samuel Fiedler <samuel.fiedler@proton.me>
2024-06-22 07:52:55 +0200

    kernel/port_io: explain

--- src/kernel/port_io.zig
+++ src/kernel/port_io.zig
@@ -1,21 +1,30 @@
-//! Port I/O functionality
-//! 2023 by Samuel Fiedler
+//! This is some really basic Port I/O stuff.
+//! Kernels have access to various ports where they can read and write data.
+//! For example, there is a keyboard port that is wired to the default keyboard.
 
-/// Input bytes
+/// This function returns a byte it got as result from an input to a port.
 pub fn inb(port: u16) u8 {
+    // We do this using the assembly instruction "inb".
     return asm volatile ("inb %[port], %[ret]"
+        // Our return value should be an 8bit unsigned integer and it should be saved in the register "al".
         : [ret] "={al}" (-> u8),
+          // The port is already specified in the function argument (so Zig knows it is 16bit wide) and it should be in the register "dx".
         : [port] "{dx}" (port),
+          // For this operation, we use the "dx" and "al" registers.
         : "dx", "al"
     );
 }
 
-/// Output bytes
+/// This function puts a byte to a specific port.
 pub fn outb(port: u16, val: u8) void {
+    // So, again our assembly.
     asm volatile ("outb %[val], %[port]"
+        // No return value
         :
+        // But two arguments
         : [val] "{al}" (val),
           [port] "{dx}" (port),
+          // And again, we use the "dx" and "al" registers.
         : "dx", "al"
     );
 }
