328c5adf9ef89f8c6382bae8cc28b28945c7ec6b
Leon Davis <leonskij@proton.me>
2025-09-03 07:34:39 +0100

    add options to pass the OVMF_CODE and OVMF_VARS files in the qemu step
    the passed OVMF_CODE and OVMF_VARS files are copied so that any edits
    do not effect the systems files. for now they are placed into the
    same directory used for the boot disk, but they can be copied to a
    different directory easily in the future if preferred.

--- build.zig
+++ build.zig
@@ -98,36 +98,95 @@ pub fn build(b: *Build) void {
     b.installArtifact(bootloader_exe);
     // This line of code installs the kernel.
     b.installArtifact(kernel_exe);
-    // After that, we create a directory in the zig cache into which we can
-    // copy files.
-    const boot_dir = b.addWriteFiles();
-    // Now, we copy the bootloader executable into a folder that will be
-    // recognized by UEFI.
-    _ = boot_dir.addCopyFile(bootloader_exe.getEmittedBin(), b.pathJoin(&.{ "efi/boot", bootloader_exe.out_filename }));
-    // Here, we copy the kernel executable to a custom location.
-    _ = boot_dir.addCopyFile(kernel_exe.getEmittedBin(), kernel_exe.out_filename);
-    // With this command, we start QEMU (a computer emulator)…
+    // Add an option to supply the OVMF_CODE file…
+    const ovmf_code = b.option(
+        Build.LazyPath,
+        "ovmf-code",
+        "The OVMF_CODE file to use",
+    );
+    // …and the same for the OVMF_VARS file.
+    const ovmf_vars = b.option(
+        Build.LazyPath,
+        "ovmf-vars",
+        "The OVMF_VARS file to use",
+    );
+    // With this command, we can start QEMU (a computer emulator).
     const qemu_cmd = b.addSystemCommand(&.{"qemu-system-x86_64"});
-    // …that depends on the bootloader and kernel install steps we defined
-    // above…
-    qemu_cmd.step.dependOn(b.getInstallStep());
-    // …with UEFI as firware using the OVMF.fd file…
-    qemu_cmd.addArg("-bios");
-    qemu_cmd.addFileArg(b.path("OVMF.fd"));
-    // …an emulated FAT drive using the directory we made in the above…
-    qemu_cmd.addArg("-hdd");
-    qemu_cmd.addPrefixedDirectoryArg("fat:rw:", boot_dir.getDirectory());
-    // …the standard output mapped to the COM1, allowing us to see messages
-    // from the operating system directly on our console…
-    qemu_cmd.addArg("-serial");
-    qemu_cmd.addArg("mon:stdio");
-    // …a GTK-based window for display…
-    qemu_cmd.addArg("-display");
-    qemu_cmd.addArg("gtk");
-    // …and a GDB (a debugger tool) remote-connection client available on
-    // localhost:1234 via TCP.
-    qemu_cmd.addArg("-s");
-    // The we create a subcommand (`zig build qemu`) to run the above system
+    // Check if the ovmf_code option was provided…
+    if (ovmf_code) |ocp| {
+        // …and then if the ovmf_vars option was too.
+        if (ovmf_vars) |ovp| {
+            // After that, we create a directory in the zig cache into which we
+            // can copy files.
+            const boot_dir = b.addWriteFiles();
+            // We copy the OVMF_CODE file…
+            const oc = boot_dir.addCopyFile(
+                ocp,
+                ocp.basename(b, &boot_dir.step),
+            );
+            // …and the OVMF_VARS file so that we do not modify the system ones
+            // when in use.
+            const ov = boot_dir.addCopyFile(
+                ovp,
+                ovp.basename(b, &boot_dir.step),
+            );
+            // Now, we copy the bootloader executable into a folder that will be
+            // recognized by UEFI.
+            _ = boot_dir.addCopyFile(
+                bootloader_exe.getEmittedBin(),
+                b.pathJoin(&.{
+                    "efi",
+                    "boot",
+                    bootloader_exe.out_filename,
+                }),
+            );
+            // Here, we copy the kernel executable to a custom location.
+            _ = boot_dir.addCopyFile(
+                kernel_exe.getEmittedBin(),
+                kernel_exe.out_filename,
+            );
+            // …with UEFI as firware using the OVMF_CODE file…
+            qemu_cmd.addArg("-drive");
+            qemu_cmd.addPrefixedFileArg(
+                "format=raw,if=pflash,readonly=on,file=",
+                oc,
+            );
+            // …and OVMF_VARS file…
+            qemu_cmd.addArg("-drive");
+            qemu_cmd.addPrefixedFileArg("format=raw,if=pflash,file=", ov);
+            // …an emulated FAT drive using the directory we made above…
+            qemu_cmd.addArg("-drive");
+            qemu_cmd.addPrefixedDirectoryArg(
+                "format=raw,index=3,media=disk,file=fat:rw:",
+                boot_dir.getDirectory(),
+            );
+            // …the standard output mapped to the COM1, allowing us to see
+            // messages from the operating system directly on our console…
+            qemu_cmd.addArg("-serial");
+            qemu_cmd.addArg("mon:stdio");
+            // …a GTK-based window for display…
+            qemu_cmd.addArg("-display");
+            qemu_cmd.addArg("gtk");
+            // …and a GDB (a debugger tool) remote-connection client available
+            // on localhost:1234 via TCP.
+            qemu_cmd.addArg("-s");
+        } else {
+            // If the ovmf_vars option was not provided, then tell the user it
+            // is required.
+            const fail = b.addFail(
+                "The -Dovmf-vars=... option is required for this step",
+            );
+            qemu_cmd.step.dependOn(&fail.step);
+        }
+    } else {
+        // If the ovmf_code option was not provided, then tell the user it is
+        // required.
+        const fail = b.addFail(
+            "The -Dovmf-code=... option is required for this step",
+        );
+        qemu_cmd.step.dependOn(&fail.step);
+    }
+    // Then we create a subcommand (`zig build qemu`) to run the above system
     // command…
     const qemu_step = b.step("qemu", "Run the kernel via QEMU");
     // …and make sure it depends on that command's step.
--- readme.md
+++ readme.md
@@ -5,15 +5,18 @@ This is a (very simple) example on how to write a bootloader that loads a kernel
 programming language ZIG.
 
 It is designed to help starters understand how an operating system works, so sources are
-explained well.  
+explained well.
 If you want to improve the explanations, feel free to open an issue or a pull request!
 
 Building and Running
 --------------------
 
-Just run `zig build` to build the bootloader and kernel. If you have `qemu-system-x86_64`
-installed, then you can also run `zig build qemu` to run the kernel. It sets up a directory
-in the build cache to use as the emulated FAT disk and runs QEMU using it.
+Just run `zig build` to build the bootloader and kernel.
+
+If you have `qemu-system-x86_64` installed, then you can run `zig build qemu` to build and
+run the kernel. It sets up a directory in the build cache to use as the emulated FAT disk
+and runs QEMU using it. You are expected to pass it the `OVMF_CODE[.m4].fd` and
+`OVMF_VARS[.m4].fd` via the `-Dovmf-code` and `-Dovmf-vars` options, respectively.
 
 As of now, the tagged Zig releases are targeted. This project is known to work with the
 following Zig versions:
@@ -25,7 +28,7 @@ following Zig versions:
 Further Information
 -------------------
 
-This work is licensed under the MIT License. Read it in the `LICENSE` file.  
-You can read the license for the `OVMF.fd` BLOB in the `OVMF.fd.LICENSE.txt` file.  
+This work is licensed under the MIT License. Read it in the `LICENSE` file.
+You can read the license for the `OVMF.fd` BLOB in the `OVMF.fd.LICENSE.txt` file.
 This repository was previously the place of development for the so-called "Loup OS", for which I
 have now created a separate organisation: <https://codeberg.org/loup-os>.
